35cf2153b892dece98186078293a2bbcb1e11520
David Hernández <d.hdza19@gmail.com>
2022-05-25 20:20:31 -0500

    Add Fetcher class

---
+++ src/movies/Fetcher.py
@@ -0,0 +1,45 @@
+# SRP - El fetcher es responsable de extraer toda la información de las pelis
+
+from typing import List
+import re
+from Downloader import Downloader
+from InterfaceFetcher import InterfaceFetcher
+
+class Fetcher(InterfaceFetcher):
+    def fetch_movies(self) -> List:
+        # create a empty list for storing
+        # movie information
+        list = []
+
+        # Get data from Downloader getters
+        movies = Downloader.movies
+        links = Downloader.links
+        crew = Downloader.crew
+        ratings = Downloader.rating
+        votes = Downloader.votes
+
+        # Iterating over movies to extract
+        # each movie's details
+        for index in range(0, len(movies)):
+            # Separating movie into: 'place',
+            # 'title', 'year'
+            movie_string = movies[index].get_text()
+            movie = (' '.join(movie_string.split()).replace('.', ''))
+            movie_title = movie[len(str(index)) + 1:-7]
+            year = re.search('\((.*?)\)', movie_string).group(1)
+            place = movie[:len(str(index)) - (len(movie))]
+
+            data = {"movie_title": movie_title,
+                    "year": year,
+                    "place": place,
+                    "star_cast": crew[index],
+                    "rating": ratings[index],
+                    "vote": votes[index],
+                    "link": links[index],
+                    "preference_key": index % 4 + 1}
+            list.append(data)
+        
+        return list
+        
+
+
