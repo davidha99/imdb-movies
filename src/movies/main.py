0e9844db633b6da08d41dc58607ec6e605691257
David Hernández <d.hdza19@gmail.com>
2022-05-29 15:10:45 -0500

    Add menu, algorithm and recommendations

--- src/movies/main.py
+++ src/movies/main.py
@@ -1,4 +1,5 @@
 import csv
+from typing import List
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from Facade import Facade
@@ -13,8 +14,34 @@ DEFAULT_SESSION_FACTORY = sessionmaker(
 )
 session = DEFAULT_SESSION_FACTORY()
 
+def print_menu():
+    print("-----------------------------------------------")
+    print("1. Comedy")
+    print("2. Drama")
+    print("3. Sci-fi")
+    print("4. Romantic")
+    print("5. Adventure")
+    print("-----------------------------------------------")
+    preference1 = int(input("Choose preference 1 (1,2,3,4,5): "))
+    preference2 = int(input("Choose preference 2 (1,2,3,4,5): "))
+    preference3 = int(input("Choose preference 3 (1,2,3,4,5): "))
+    return preference1, preference2, preference3
+
+def algorithm(p1: int, p2: int, p3: int) -> int:
+    return ((p1 * p2 * p3) % 5) + 1
+
+def print_recommendations(movie_list: List, user_preference_key: int) -> None:
+    n = 1
+    for movie in movie_list:
+        title = movie["movie_title"]
+        movie_preference_key = movie["preference_key"]
+        if user_preference_key == movie_preference_key:
+            print(f"{n}: {title}")
+            n += 1
 
 def main():
+    preference1, preference2, preference3 = print_menu()
+    user_preference_key = algorithm(preference1, preference2, preference3)
 
     #Interface Segregation - By not communicating directly to the concrete classes, we can assure interface segregation because
     #we will only be able to use methods declared in Facade. If Fetcher had more methods declared, we will not be able to access them
@@ -25,6 +52,9 @@ def main():
     facade = Facade()
     facade.download_data()
     list = facade.fetch_movies()
+
+    # Print final movie list from user preferences
+    print_recommendations()
 
     # Factory
     fields = ["preference_key", "movie_title", "star_cast", "rating", "year", "place", "vote", "link"]
